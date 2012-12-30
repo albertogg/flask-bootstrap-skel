d0d4b115878a37bad6e855c901dee3d77fbf053f
albertogg <albertogrespan@gmail.com>
2012-12-30 18:41:17 -0430

    Better tests.

--- application/default_settings.py
+++ application/default_settings.py
@@ -1,4 +1,9 @@
+import os
+
+# Get application base dir.
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 RELOAD = True
 SECRET_KEY = 'mysecretkeyvalue'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/mydevdatabase'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(_basedir, 'db/app_dev.db')
--- tests/manage_tests.py
+++ tests/manage_tests.py
@@ -1,19 +1,34 @@
+import os
 import unittest
-from application import app
+from application.default_settings import _basedir
+from application import app, db
 
 
 class ManagerTestCase(unittest.TestCase):
+    """ setup and teardown for the testing database """
 
     def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:////'
+                                    + os.path.join(_basedir, 'db/tests.db'))
         self.app = app.test_client()
+        db.create_all()
 
-    def t_username(self, username):
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+class OriginalRoutes(ManagerTestCase):
+    """ test suite for the original in app routes """
+
+    def route_username(self, username):
         return self.app.get('/hello/%s' % (username), follow_redirects=True)
 
     def test_username(self):
-        rv = self.t_username('alberto')
+        rv = self.route_username('alberto')
         assert "Hello, alberto" in rv.data
 
-    def test_empty_db(self):
+    def test_index(self):
         rv = self.app.get('/')
         assert 'hi' in rv.data
