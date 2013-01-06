3cdf3eb9166dd196cacba48b58162841981d36fe
albertogg <albertogrespan@gmail.com>
2013-01-06 15:20:48 -0430

    test db fix again

--- tests/manage_tests.py
+++ tests/manage_tests.py
@@ -8,6 +8,9 @@ class ManagerTestCase(unittest.TestCase):
     """ setup and teardown for the testing database """
 
     def setUp(self):
+        create_db_dir = _basedir + '/db'
+        if not os.path.exists(create_db_dir):
+            os.mkdir(create_db_dir, 0755)
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'
                                     + os.path.join(_basedir, 'db/tests.db'))
