507323b98d5fc708f20dfaf439ddf7b1136b8270
albertogg <albertogrespan@gmail.com>
2013-01-14 02:53:30 -0430

    New error templates
    * Fix routes to work with url_for.
    * New 500 error page and handler.
    * New 404 error page and handler.
    
    [ci skip]

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask.ext.sqlalchemy import SQLAlchemy
 
 # Create the app and configuration
@@ -14,7 +14,16 @@ db = SQLAlchemy(app)
 # 404 page not found "route"
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    title = "404 Page not found :("
+    return render_template('404.html', title=title), 404
+
+
+# 500 server error "route"
+@app.errorhandler(500)
+def server_error(error):
+    title = "500 Server Error :("
+    db.session.rollback()
+    return render_template('500.html', title=title), 500
 
 
 import application.manager
--- application/manager.py
+++ application/manager.py
@@ -4,12 +4,12 @@ from application.models import *
 
 
 @app.route('/')
-@app.route('/index')
-def hello():
+@app.route('/index/')
+def index():
     return render_template('info/index.html', title='Flask-Bootstrap')
 
 
-@app.route('/hello/<username>')
+@app.route('/hello/<username>/')
 def hello_username(username):
     return render_template('info/hello.html', title="Flask-Bootstrap, Hi %s"
                             % (username), username=username)
