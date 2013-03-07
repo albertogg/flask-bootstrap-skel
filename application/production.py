7bca12a949097348606bfe0476f720ed1a229e86
albertogg <albertogrespan@gmail.com>
2013-03-07 01:59:15 -0430

    Change environment variable.
    Set the correct environment variable for the primary heroku postgresql database.

--- application/production.py
+++ application/production.py
@@ -4,4 +4,4 @@ DEBUG = False
 RELOAD = False
 CSRF_ENABLED = True
 SECRET_KEY = 'notmysecretkey'
-SQLALCHEMY_DATABASE_URI = str(os.environ.get('HEROKU_POSTGRESQL', 'postgresql://localhost/myproddatabase'))
+SQLALCHEMY_DATABASE_URI = str(os.environ.get('DATABASE_URL', 'postgresql://localhost/myproddatabase'))
